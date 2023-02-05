c8efa72c05a115504c7758bafcea3a2d52965f43
dannyoma75@gmail.com <dannyoma75@gmail.com>
2023-02-05 11:49:54 +0100

    +*interval publishing of topic and checking

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://broker.hivemq.com')
 
 client.on('connect', function () {
-  client.publish('vertgrid/ESP/LED1',"true");
-  client.publish('vertgrid/ESP/LED2',"false");
+  
+  setInterval(()=>{
+    client.publish('vertgrid/ESP/LED1',"true");
+    client.publish('vertgrid/ESP/LED2',"false");
+  },2000)
+ 
+  
   client.subscribe("vertgrid/esp/temperature",(e)=>{
     e? console.log(e):"";
   });
   client.subscribe("vertgrid/esp/humidity",(e)=>{
     e? console.log(e):'';
   })
-  client.subscribe('vertgrid/presence', function (err) {
+  client.subscribe('vertgrid/ESP/LED1', function (err) {
     if (!err) {
-      client.publish('vertgrid/presence', 'Hello mqtt')
+      
     }
   })
-  client.subscribe("vertgrid/testData", (e)=>{
+  client.subscribe("vertgrid/ESP/LED2", (e)=>{
     e?console.log(e):"";
   })
 })
